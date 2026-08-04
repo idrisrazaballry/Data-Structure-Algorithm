ac17b84dd2713ab9c251fb4863601106854d34de
Idrisraza Ballary <152577081+idrisrazaballry@users.noreply.github.com>
2026-08-04 19:09:23 +0530

    Add stack implementation to reverse a string
    Implemented a stack-based approach to reverse a string input by the user.

--- test2.py
+++ test2.py
@@ -1,1 +1,27 @@
+# WAP to reverse the given string using stack implementation
 
+class stack:
+    def __init__(self):
+        self.s1 = []
+
+    def push(self, val):
+        self.s1.append(val)
+    
+    def pop(self):
+        if len(self.s1) == 0:
+            return False
+        else:
+            return self.s1.pop()
+
+s = stack()
+str = input("Enter the string: ")
+print("Given string --> ", str)
+for i in range(len(str)):
+    s.push(str[i])
+
+print()
+
+res = ""
+while (len(s.s1)) != 0:
+    res += s.pop()
+print("Reversed string --> ",res)
