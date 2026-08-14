fd342f7bba07fd313ae1af534f99fab677872da4
Idrisraza Ballary <152577081+idrisrazaballry@users.noreply.github.com>
2026-08-15 00:46:26 +0530

    Implement stair climbing problem solution
    Added a program to calculate the number of ways to climb stairs based on user input.

--- stairsProblem.py
+++ stairsProblem.py
@@ -1,1 +1,16 @@
+# WAP to find the total number of ways to climb stairs
 
+n = int(input("Enter the number of stairs: "))
+
+if n == 0 or n == 1:
+    print(1)
+else:
+    first = 1
+    second = 1
+
+    for i in range(2, n + 1):
+        total = first + second
+        first = second
+        second = total
+
+    print(second)
